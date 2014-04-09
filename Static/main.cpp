6d562062cdc1f9b630d809bb52d10f043de110a0
tanmum <buihaitan89@gmail.com>
2014-04-09 01:15:05 -0500

    Initial Commit

---
+++ Static/main.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  Static
+//
+//  Created by Tan Bui on 4/9/14.
+//  Copyright (c) 2014 tanbui. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
+
